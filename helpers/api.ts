a6b6be53e3cffa7d87f58df10042482e9307f71c
Frank Mwesigwa <frank.mwesigwa1@gmail.com>
2025-09-02 17:24:04 +0300

    fixed deployment issues

--- helpers/api.ts
+++ helpers/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: process.env.NODE_ENV === 'production' 
-    ? process.env.NEXT_PUBLIC_API_URL_PROD
-    : process.env.NEXT_PUBLIC_API_URL_DEV
+    ? process.env.NEXT_PUBLIC_API_URL_PROD || 'https://conference.health.go.ug/api'
+    : process.env.NEXT_PUBLIC_API_URL_DEV || 'http://localhost:5000/api'
 });
 
 export default API;
